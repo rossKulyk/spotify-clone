ea5d9f61e850218a16df76c6c022fc3342f43c6a
Ross Kulyk <uaross@hotmail.com>
2022-02-18 09:38:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+// instantiate the Express app
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Good morning beautiful  people");
+});
+
+// listen for a connection on port 8080
+const port = 8080;
+app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`);
+});
