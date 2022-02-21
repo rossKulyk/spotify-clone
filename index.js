5ee35e45fd043713a2a0c4a0d0165887404b088d
Ross Kulyk <uaross@hotmail.com>
2022-02-21 10:35:19 -0500

    setup of /login route with redirect to Spotify account service

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 require("dotenv").config();
-console.log("process.env.CLIENT_ID >", process.env.CLIENT_ID);
+// console.log("process.env.CLIENT_ID >", process.env.CLIENT_ID);
+const querystring = require("querystring"); // allows to parse and stringify query strings
 
 const express = require("express");
 // instantiate the Express app
 const app = express();
 
+// store env variable
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
 app.get("/", (req, res) => {
     res.send("Good morning beautiful  people");
+});
+
+// The redirect URI is a route of our app that we want the Spotify Accounts Service to redirect the user to once they've authorized our app (i.e. successfully logged into Spotify).
+
+// login route to redirect to the Spotify Accounts Service with client_id, redicrect_uri
+app.get("/login", (req, res) => {
+    const queryParams = querystring.stringify({
+        client_id: CLIENT_ID,
+        response_type: "code",
+        redirect_uri: REDIRECT_URI
+    });
+
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
 // listen for a connection on port 8080
